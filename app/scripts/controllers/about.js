69ae798c31b3b367523cfe1073f829e778aa682d
miamarti <miller.augusto@gmail.com>
2015-02-02 18:01:29 -0200

    Start Project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name umbandaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the umbandaApp
+ */
+angular.module('umbandaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
